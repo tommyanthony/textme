325334c37e1ef3cadadf6be7809e54a8121f468d
Thomas Anthony <anthony.p.tommy@gmail.com>
2015-10-10 13:57:57 -0700

    Griff made parsing requests work

--- http_requests.py
+++ http_requests.py
@@ -1,19 +1,19 @@
 from parsing import interpret_request
 import requests
-import http.client, urllib.parse
 
-service_to_gram = {'google':['{str:"maps"}, {str:}, {str:}']}
-gram_to_endpoint = {'{str:"maps"}, {str:}, {str:}':'http://127.0.0.1:5000/directions/{1}/{2}'}       
+service_to_gram = {'google':['{str:"maps"},{str:},{str:}']}
+gram_to_endpoint = {'{str:"maps"},{str:},{str:}':'http://127.0.0.1:5000/directions/{1}/{2}'}
 
-# cannot have more than 10 params!
+
 def process_request(id_num, phone, body):
     service = body.split(',')[0]
     if service not in list(service_to_gram.keys()):
         return "ERROR - no such service"
     grammars = service_to_gram[service]
     for g in grammars:
-        params = interpret_request(body)
+        params = interpret_request(body, g)
         if type(params) != str:
+            print(params)
             endpoint = gram_to_endpoint[g]
             i = 0
             while i < len(endpoint):
@@ -22,13 +22,12 @@ def process_request(id_num, phone, body):
                 i += 1
             return http_request(endpoint, params)
 
-def http_request(url, body):
-    payload = {'body': body}
-    return requests.get(url, data=payload)
 
-def old_http_request(url, body):
-    params = urllib.parse.urlencode({'@body': body})
-    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-    conn = http.client.HTTPConnection(url)
-    conn.request("GET", "", params, headers)
-    return conn.getresponse()
+def http_request(url, body):
+    print("Connecting to url %s" % url)
+    payload = {'body': body}
+    return requests.get(url, data=payload).text
+
+if __name__ == "__main__":
+    print(process_request("TESTING123", "310-362-347",
+                          "google, maps, Manhattan Beach, Berkeley"))
