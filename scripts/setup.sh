fcc82cb6f50c933fbaad390c3ca4c59aa2473724
Thomas Anthony <anthony.p.tommy@gmail.com>
2015-10-10 15:49:38 -0700

    Updated scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,5 +2,5 @@ cd ../
 if [ ! -d logs ]; then
     mkdir logs
 fi
-python gmaps_client/gmaps.py > logs/gmaps_out.txt &
-python sms_client.py > logs/sms_client_out.txt &
+nohup python gmaps_client/gmaps.py > logs/gmaps_out.txt &
+nohup python sms_client.py > logs/sms_client_out.txt &
--- scripts/worker.sh
+++ scripts/worker.sh
@@ -1,1 +1,4 @@
-python rqworker.py > logs/worker.txt &
+for i in $(seq ${1-1})
+do
+    nohup python3 rqworker.py > logs/worker$i.txt &
+done
