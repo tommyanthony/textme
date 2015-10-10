1b693ecb4e712657d174f52175acfeba7d317b3f
Thomas Anthony <anthony.p.tommy@gmail.com>
2015-10-10 03:07:31 -0700

    Added an Endpoints database and a super easy way to create the database! Run python db/schema.py create to create the db on your machine

--- db/schema.py
+++ db/schema.py
@@ -1,24 +1,34 @@
-from sqlalchemy import (Column, ForeignKey, Integer, DECIMAL, String, BOOLEAN,
+from sqlalchemy import (Column, ForeignKey, Integer, String, BOOLEAN,
                         create_engine)
-from sqlalchemy.ext.declarative import declarative_base, declared_attr
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import database_exists, create_database
 
 
 Base = declarative_base()
 
-class RecievedSms(Base):
-    __tablename__ = 'recieved'
+class ReceivedSms(Base):
+    __tablename__ = 'received'
     id = Column(String(34), primary_key=True)
     from_num = Column(String(100), nullable=False)
     body = Column(String(1600), nullable=False)
     resolved = Column(BOOLEAN)
 
 class Response(Base):
+    __tablename__ = 'response'
     id = Column(Integer, primary_key=True)
-    sms_id = Column(Integer, ForeignKey(RecievedSms.__tablename__ + ".id"))
+    sms_id = Column(String(34), ForeignKey(ReceivedSms.__tablename__ + ".id"))
     to_num = Column(String(100), nullable=False)
     body = Column(String(1600), nullable=False)
     sent = Column(BOOLEAN)
+
+class Endpoints(Base):
+    __tablename__ = 'endpoints'
+    id = Column(Integer, primary_key=True)
+    grammar = Column(String(300), nullable=False)
+    endpoint = Column(String(250), nullable=False)
+    service = Column(String(100), nullable=False)
+
 
 DB_NAME = 'twilio'
 SQL_TYPE = 'mysql'
@@ -45,4 +55,17 @@ def generate_url(db_name=DB_NAME, sql_type=SQL_TYPE, driver=DRIVER, host=HOST,
 def create_tables(db_url=None):
     if db_url is None:
         db_url = generate_url()
+    engine = create_engine(db_url)
+    if not database_exists(engine.url):
+        create_database(engine.url)
     Base.metadata.create_all(create_engine(db_url))
+
+if __name__ == "__main__":
+    import sys
+    args = sys.argv[1:]
+    arg = args[0]
+    if arg == "create":
+        if len(args) > 1:
+            create_tables(args[1])
+        else:
+            create_tables()
