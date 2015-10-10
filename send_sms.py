9174de67cf3b6c7f4b0cbb3c58b73734110a32fb
Thomas Anthony <anthony.p.tommy@gmail.com>
2015-10-10 11:40:52 -0700

    Added a method for sending SMS texts with twilio

--- send_sms.py
+++ send_sms.py
@@ -4,7 +4,14 @@ from twilio.rest import TwilioRestClient
 # Find these values at https://twilio.com/user/account
 account_sid = "ACXXXXXXXXXXXXXXXXX"
 auth_token = "YYYYYYYYYYYYYYYYYY"
+FROM_NUMBER = "+15555555555"
 client = TwilioRestClient(account_sid, auth_token)
- 
-message = client.messages.create(to="+12316851234", from_="+15555555555",
-                                     body="Hello there!")
+
+def send_message(to_num, body):
+    """
+    Sends a message to TO_NUM with a message body of BODY
+    """
+    message = client.messages.create(to=to_num, from_=FROM_NUMBER, body=body)
+
+message = client.messages.create(to="+12316851234", from_=FROM_NUMBER,
+                                 body="Hello there!")
