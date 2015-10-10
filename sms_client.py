7b894cced4eb03279d52bae58a3137e3dc610a7f
Thomas Anthony <anthony.p.tommy@gmail.com>
2015-10-10 01:29:55 -0700

    Added Celerey to requirements.txt. Added the basic template for sms_client.py.

--- sms_client.py
+++ sms_client.py
@@ -5,12 +5,28 @@ app = Flask(__name__)
 
 
 @app.route("/twilio", methods=['GET', 'POST'])
-def hello_monkey():
-    """Respond to incoming calls with a simple text message."""
+def recieve_sms():
+    """
+    Recieves SMS text messages from Twilio, and inserts them into the MySQL
+    database for Celery to pick up and execute on. Logs a warning if there is
+    none of From, MessageSid, or Body in the request's parameters.
+    """
 
-    resp = twilio.twiml.Response()
-    resp.message("Hello, Mobile Monkey")
-    return str(resp)
+    from_number = param('From')
+    unique_id = param('MessageSid')
+    body = param('Body')
+    if from_number and unique_id and body:
+        # insert into DB for Celery to pick up
+        pass
+    else:
+        # error?
+        resp = twilio.twiml.Response()
+        resp.message("Hello, Mobile Monkey")
+        return str(resp)
+
+
+def param(name):
+    return request.values.get(name, None)
 
 if __name__ == "__main__":
     app.run(debug=True)
