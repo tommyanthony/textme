7c4514b4d46ee012372f1f7712000b5b9fbcafe9
Griffin Potrock <potrock@berkeley.edu>
2015-10-10 13:02:21 -0700

    tabs->spaces

--- http_requests.py
+++ http_requests.py
@@ -2,28 +2,28 @@ import parsing
 import http.client, urllib.parse
 
 service_to_gram = {}
-gram_to_endpoint = {}		
+gram_to_endpoint = {}       
 
 # cannot have more than 10 params!
 def process_request(id_num, phone, body):
-	service = body.split(',')[0]
-	if service not in list(service_to_gram.keys()):
-		return "ERROR - no such service"
-	grammars = service_to_gram[service]
-	for g in grammars:
-		params = interpret_request(body)
-		if type(params) != str:
-			endpoint = gram_to_endpoint[g]
-			i = 0
-			while i < len(endpoint):
-				if endpoint[i] == '{':
-					endpoint = endpoint[:i] + params[int(endpoint[i+1])] + endpoint[i+3:]
-				i += 1
-			return http_request(endpoint, params)
+    service = body.split(',')[0]
+    if service not in list(service_to_gram.keys()):
+        return "ERROR - no such service"
+    grammars = service_to_gram[service]
+    for g in grammars:
+        params = interpret_request(body)
+        if type(params) != str:
+            endpoint = gram_to_endpoint[g]
+            i = 0
+            while i < len(endpoint):
+                if endpoint[i] == '{':
+                    endpoint = endpoint[:i] + params[int(endpoint[i+1])] + endpoint[i+3:]
+                i += 1
+            return http_request(endpoint, params)
 
 def http_request(url, body):
-	params = urllib.parse.urlencode({'@body': body})
-	headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-	conn = http.client.HTTPConnection(url)
-	conn.request("GET", "", params, headers)
-	return conn.getresponse()
+    params = urllib.parse.urlencode({'@body': body})
+    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+    conn = http.client.HTTPConnection(url)
+    conn.request("GET", "", params, headers)
+    return conn.getresponse()
